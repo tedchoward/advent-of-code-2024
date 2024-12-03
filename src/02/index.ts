f9283e74d9bbfbb202c6a3672056c00accbf0278
Ted C. Howard <ted@tedchoward.com>
2024-12-03 10:29:04 -0600

    use `performance.now()` instead of `Date.now()`

--- src/02/index.ts
+++ src/02/index.ts
@@ -2,10 +2,14 @@ import { calculatePart1, calculatePart2 } from './lib';
 
 const input = await Bun.file(`${import.meta.dir}/input.txt`).text();
 
-let start = Date.now();
+let start = performance.now();
 console.log(`Part 1: ${calculatePart1(input)}`);
-console.log(`\ttook ${Date.now() - start}ms`);
+console.log(
+  `\ttook ${Math.round((performance.now() - start) * 1000) / 1000}ms`,
+);
 
-start = Date.now();
+start = performance.now();
 console.log(`Part 2: ${calculatePart2(input)}`);
-console.log(`\ttook ${Date.now() - start}ms`);
+console.log(
+  `\ttook ${Math.round((performance.now() - start) * 1000) / 1000}ms`,
+);
